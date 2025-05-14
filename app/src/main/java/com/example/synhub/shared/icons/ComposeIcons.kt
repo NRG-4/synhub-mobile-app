ab74f8b4bcdde6d97c90e1de1dadaaebbcd7bec0
Al3xiel <gabriel280104casas@gmail.com>
2025-05-14 00:20:49 -0500

    feat(slide-menu): added slide menu.

--- app/src/main/java/com/example/synhub/shared/icons/ComposeIcons.kt
+++ app/src/main/java/com/example/synhub/shared/icons/ComposeIcons.kt
@@ -346,3 +346,438 @@ val linkSVG: ImageVector
         }.build()
         return _linkSVG!!
     }
+private var _groupSVG: ImageVector? = null
+public val groupSVG: ImageVector
+    get() {
+        if (_groupSVG != null) {
+            return _groupSVG!!
+        }
+        _groupSVG = ImageVector.Builder(
+            name = "Groups",
+            defaultWidth = 24.dp,
+            defaultHeight = 24.dp,
+            viewportWidth = 960f,
+            viewportHeight = 960f
+        ).apply {
+            path(
+                fill = SolidColor(Color.Black),
+                fillAlpha = 1.0f,
+                stroke = null,
+                strokeAlpha = 1.0f,
+                strokeLineWidth = 1.0f,
+                strokeLineCap = StrokeCap.Butt,
+                strokeLineJoin = StrokeJoin.Miter,
+                strokeLineMiter = 1.0f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(0f, 720f)
+                verticalLineToRelative(-63f)
+                quadToRelative(0f, -43f, 44f, -70f)
+                reflectiveQuadToRelative(116f, -27f)
+                quadToRelative(13f, 0f, 25f, 0.5f)
+                reflectiveQuadToRelative(23f, 2.5f)
+                quadToRelative(-14f, 21f, -21f, 44f)
+                reflectiveQuadToRelative(-7f, 48f)
+                verticalLineToRelative(65f)
+                close()
+                moveToRelative(240f, 0f)
+                verticalLineToRelative(-65f)
+                quadToRelative(0f, -32f, 17.5f, -58.5f)
+                reflectiveQuadTo(307f, 550f)
+                reflectiveQuadToRelative(76.5f, -30f)
+                reflectiveQuadToRelative(96.5f, -10f)
+                quadToRelative(53f, 0f, 97.5f, 10f)
+                reflectiveQuadToRelative(76.5f, 30f)
+                reflectiveQuadToRelative(49f, 46.5f)
+                reflectiveQuadToRelative(17f, 58.5f)
+                verticalLineToRelative(65f)
+                close()
+                moveToRelative(540f, 0f)
+                verticalLineToRelative(-65f)
+                quadToRelative(0f, -26f, -6.5f, -49f)
+                reflectiveQuadTo(754f, 563f)
+                quadToRelative(11f, -2f, 22.5f, -2.5f)
+                reflectiveQuadToRelative(23.5f, -0.5f)
+                quadToRelative(72f, 0f, 116f, 26.5f)
+                reflectiveQuadToRelative(44f, 70.5f)
+                verticalLineToRelative(63f)
+                close()
+                moveToRelative(-455f, -80f)
+                horizontalLineToRelative(311f)
+                quadToRelative(-10f, -20f, -55.5f, -35f)
+                reflectiveQuadTo(480f, 590f)
+                reflectiveQuadToRelative(-100.5f, 15f)
+                reflectiveQuadToRelative(-54.5f, 35f)
+                moveTo(160f, 520f)
+                quadToRelative(-33f, 0f, -56.5f, -23.5f)
+                reflectiveQuadTo(80f, 440f)
+                quadToRelative(0f, -34f, 23.5f, -57f)
+                reflectiveQuadToRelative(56.5f, -23f)
+                quadToRelative(34f, 0f, 57f, 23f)
+                reflectiveQuadToRelative(23f, 57f)
+                quadToRelative(0f, 33f, -23f, 56.5f)
+                reflectiveQuadTo(160f, 520f)
+                moveToRelative(640f, 0f)
+                quadToRelative(-33f, 0f, -56.5f, -23.5f)
+                reflectiveQuadTo(720f, 440f)
+                quadToRelative(0f, -34f, 23.5f, -57f)
+                reflectiveQuadToRelative(56.5f, -23f)
+                quadToRelative(34f, 0f, 57f, 23f)
+                reflectiveQuadToRelative(23f, 57f)
+                quadToRelative(0f, 33f, -23f, 56.5f)
+                reflectiveQuadTo(800f, 520f)
+                moveToRelative(-320f, -40f)
+                quadToRelative(-50f, 0f, -85f, -35f)
+                reflectiveQuadToRelative(-35f, -85f)
+                quadToRelative(0f, -51f, 35f, -85.5f)
+                reflectiveQuadToRelative(85f, -34.5f)
+                quadToRelative(51f, 0f, 85.5f, 34.5f)
+                reflectiveQuadTo(600f, 360f)
+                quadToRelative(0f, 50f, -34.5f, 85f)
+                reflectiveQuadTo(480f, 480f)
+                moveToRelative(0f, -80f)
+                quadToRelative(17f, 0f, 28.5f, -11.5f)
+                reflectiveQuadTo(520f, 360f)
+                reflectiveQuadToRelative(-11.5f, -28.5f)
+                reflectiveQuadTo(480f, 320f)
+                reflectiveQuadToRelative(-28.5f, 11.5f)
+                reflectiveQuadTo(440f, 360f)
+                reflectiveQuadToRelative(11.5f, 28.5f)
+                reflectiveQuadTo(480f, 400f)
+                moveToRelative(0f, -40f)
+            }
+        }.build()
+        return _groupSVG!!
+    }
+private var _membersSVG: ImageVector? = null
+public val membersSVG: ImageVector
+    get() {
+        if (_membersSVG != null) {
+            return _membersSVG!!
+        }
+        _membersSVG = ImageVector.Builder(
+            name = "PersonBoundingBox",
+            defaultWidth = 20.dp,
+            defaultHeight = 20.dp,
+            viewportWidth = 16f,
+            viewportHeight = 16f
+        ).apply {
+            path(
+                fill = SolidColor(Color(0xFF000000)),
+                fillAlpha = 1.0f,
+                stroke = null,
+                strokeAlpha = 1.0f,
+                strokeLineWidth = 1.0f,
+                strokeLineCap = StrokeCap.Butt,
+                strokeLineJoin = StrokeJoin.Miter,
+                strokeLineMiter = 1.0f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(1.5f, 1f)
+                arcToRelative(0.5f, 0.5f, 0f, false, false, -0.5f, 0.5f)
+                verticalLineToRelative(3f)
+                arcToRelative(0.5f, 0.5f, 0f, false, true, -1f, 0f)
+                verticalLineToRelative(-3f)
+                arcTo(1.5f, 1.5f, 0f, false, true, 1.5f, 0f)
+                horizontalLineToRelative(3f)
+                arcToRelative(0.5f, 0.5f, 0f, false, true, 0f, 1f)
+                close()
+                moveTo(11f, 0.5f)
+                arcToRelative(0.5f, 0.5f, 0f, false, true, 0.5f, -0.5f)
+                horizontalLineToRelative(3f)
+                arcTo(1.5f, 1.5f, 0f, false, true, 16f, 1.5f)
+                verticalLineToRelative(3f)
+                arcToRelative(0.5f, 0.5f, 0f, false, true, -1f, 0f)
+                verticalLineToRelative(-3f)
+                arcToRelative(0.5f, 0.5f, 0f, false, false, -0.5f, -0.5f)
+                horizontalLineToRelative(-3f)
+                arcToRelative(0.5f, 0.5f, 0f, false, true, -0.5f, -0.5f)
+                moveTo(0.5f, 11f)
+                arcToRelative(0.5f, 0.5f, 0f, false, true, 0.5f, 0.5f)
+                verticalLineToRelative(3f)
+                arcToRelative(0.5f, 0.5f, 0f, false, false, 0.5f, 0.5f)
+                horizontalLineToRelative(3f)
+                arcToRelative(0.5f, 0.5f, 0f, false, true, 0f, 1f)
+                horizontalLineToRelative(-3f)
+                arcTo(1.5f, 1.5f, 0f, false, true, 0f, 14.5f)
+                verticalLineToRelative(-3f)
+                arcToRelative(0.5f, 0.5f, 0f, false, true, 0.5f, -0.5f)
+                moveToRelative(15f, 0f)
+                arcToRelative(0.5f, 0.5f, 0f, false, true, 0.5f, 0.5f)
+                verticalLineToRelative(3f)
+                arcToRelative(1.5f, 1.5f, 0f, false, true, -1.5f, 1.5f)
+                horizontalLineToRelative(-3f)
+                arcToRelative(0.5f, 0.5f, 0f, false, true, 0f, -1f)
+                horizontalLineToRelative(3f)
+                arcToRelative(0.5f, 0.5f, 0f, false, false, 0.5f, -0.5f)
+                verticalLineToRelative(-3f)
+                arcToRelative(0.5f, 0.5f, 0f, false, true, 0.5f, -0.5f)
+            }
+            path(
+                fill = SolidColor(Color(0xFF000000)),
+                fillAlpha = 1.0f,
+                stroke = null,
+                strokeAlpha = 1.0f,
+                strokeLineWidth = 1.0f,
+                strokeLineCap = StrokeCap.Butt,
+                strokeLineJoin = StrokeJoin.Miter,
+                strokeLineMiter = 1.0f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(3f, 14f)
+                reflectiveCurveToRelative(-1f, 0f, -1f, -1f)
+                reflectiveCurveToRelative(1f, -4f, 6f, -4f)
+                reflectiveCurveToRelative(6f, 3f, 6f, 4f)
+                reflectiveCurveToRelative(-1f, 1f, -1f, 1f)
+                close()
+                moveToRelative(8f, -9f)
+                arcToRelative(3f, 3f, 0f, true, true, -6f, 0f)
+                arcToRelative(3f, 3f, 0f, false, true, 6f, 0f)
+            }
+        }.build()
+        return _membersSVG!!
+    }
+private var _tasksSVG: ImageVector? = null
+public val tasksSVG: ImageVector
+    get() {
+        if (_tasksSVG != null) {
+            return _tasksSVG!!
+        }
+        _tasksSVG = ImageVector.Builder(
+            name = "ClipboardDocumentCheck",
+            defaultWidth = 24.dp,
+            defaultHeight = 24.dp,
+            viewportWidth = 24f,
+            viewportHeight = 24f
+        ).apply {
+            path(
+                fill = null,
+                fillAlpha = 1.0f,
+                stroke = SolidColor(Color(0xFF0F172A)),
+                strokeAlpha = 1.0f,
+                strokeLineWidth = 1.5f,
+                strokeLineCap = StrokeCap.Round,
+                strokeLineJoin = StrokeJoin.Round,
+                strokeLineMiter = 1.0f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(11.3495f, 3.83619f)
+                curveTo(11.2848f, 4.046f, 11.25f, 4.2689f, 11.25f, 4.5f)
+                curveTo(11.25f, 4.9142f, 11.5858f, 5.25f, 12f, 5.25f)
+                horizontalLineTo(16.5f)
+                curveTo(16.9142f, 5.25f, 17.25f, 4.9142f, 17.25f, 4.5f)
+                curveTo(17.25f, 4.2689f, 17.2152f, 4.046f, 17.1505f, 3.8362f)
+                moveTo(11.3495f, 3.83619f)
+                curveTo(11.6328f, 2.9176f, 12.4884f, 2.25f, 13.5f, 2.25f)
+                horizontalLineTo(15f)
+                curveTo(16.0116f, 2.25f, 16.8672f, 2.9176f, 17.1505f, 3.8362f)
+                moveTo(11.3495f, 3.83619f)
+                curveTo(10.9739f, 3.8586f, 10.5994f, 3.8853f, 10.2261f, 3.9163f)
+                curveTo(9.095f, 4.0102f, 8.25f, 4.9732f, 8.25f, 6.1082f)
+                verticalLineTo(8.25f)
+                moveTo(17.1505f, 3.83619f)
+                curveTo(17.5261f, 3.8586f, 17.9006f, 3.8853f, 18.2739f, 3.9163f)
+                curveTo(19.405f, 4.0102f, 20.25f, 4.9732f, 20.25f, 6.1082f)
+                verticalLineTo(16.5f)
+                curveTo(20.25f, 17.7426f, 19.2426f, 18.75f, 18f, 18.75f)
+                horizontalLineTo(15.75f)
+                moveTo(8.25f, 8.25f)
+                horizontalLineTo(4.875f)
+                curveTo(4.2537f, 8.25f, 3.75f, 8.7537f, 3.75f, 9.375f)
+                verticalLineTo(20.625f)
+                curveTo(3.75f, 21.2463f, 4.2537f, 21.75f, 4.875f, 21.75f)
+                horizontalLineTo(14.625f)
+                curveTo(15.2463f, 21.75f, 15.75f, 21.2463f, 15.75f, 20.625f)
+                verticalLineTo(18.75f)
+                moveTo(8.25f, 8.25f)
+                horizontalLineTo(14.625f)
+                curveTo(15.2463f, 8.25f, 15.75f, 8.7537f, 15.75f, 9.375f)
+                verticalLineTo(18.75f)
+                moveTo(7.5f, 15.75f)
+                lineTo(9f, 17.25f)
+                lineTo(12f, 13.5f)
+            }
+        }.build()
+        return _tasksSVG!!
+    }
+private var _reportsSVG: ImageVector? = null
+public val reportsSVG: ImageVector
+    get() {
+        if (_reportsSVG != null) {
+            return _reportsSVG!!
+        }
+        _reportsSVG = ImageVector.Builder(
+            name = "Table_chart_view",
+            defaultWidth = 24.dp,
+            defaultHeight = 24.dp,
+            viewportWidth = 960f,
+            viewportHeight = 960f
+        ).apply {
+            path(
+                fill = SolidColor(Color.Black),
+                fillAlpha = 1.0f,
+                stroke = null,
+                strokeAlpha = 1.0f,
+                strokeLineWidth = 1.0f,
+                strokeLineCap = StrokeCap.Butt,
+                strokeLineJoin = StrokeJoin.Miter,
+                strokeLineMiter = 1.0f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(296f, 880f)
+                lineToRelative(-56f, -56f)
+                lineToRelative(276f, -277f)
+                lineToRelative(140f, 140f)
+                lineToRelative(207f, -207f)
+                lineToRelative(57f, 57f)
+                lineToRelative(-264f, 263f)
+                lineToRelative(-140f, -140f)
+                close()
+                moveToRelative(-136f, -40f)
+                quadToRelative(-33f, 0f, -56.5f, -23.5f)
+                reflectiveQuadTo(80f, 760f)
+                verticalLineToRelative(-560f)
+                quadToRelative(0f, -33f, 23.5f, -56.5f)
+                reflectiveQuadTo(160f, 120f)
+                horizontalLineToRelative(560f)
+                quadToRelative(33f, 0f, 56.5f, 23.5f)
+                reflectiveQuadTo(800f, 200f)
+                verticalLineToRelative(168f)
+                horizontalLineTo(160f)
+                close()
+                moveToRelative(0f, -552f)
+                horizontalLineToRelative(560f)
+                verticalLineToRelative(-88f)
+                horizontalLineTo(160f)
+                close()
+                moveToRelative(0f, 0f)
+                verticalLineToRelative(-88f)
+                close()
+            }
+        }.build()
+        return _reportsSVG!!
+    }
+private var _requestSVG: ImageVector? = null
+public val requestSVG: ImageVector
+    get() {
+        if (_requestSVG != null) {
+            return _requestSVG!!
+        }
+        _requestSVG = ImageVector.Builder(
+            name = "Forward_to_inbox",
+            defaultWidth = 24.dp,
+            defaultHeight = 24.dp,
+            viewportWidth = 960f,
+            viewportHeight = 960f
+        ).apply {
+            path(
+                fill = SolidColor(Color.Black),
+                fillAlpha = 1.0f,
+                stroke = null,
+                strokeAlpha = 1.0f,
+                strokeLineWidth = 1.0f,
+                strokeLineCap = StrokeCap.Butt,
+                strokeLineJoin = StrokeJoin.Miter,
+                strokeLineMiter = 1.0f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(480f, 520f)
+                lineTo(160f, 320f)
+                verticalLineToRelative(400f)
+                horizontalLineToRelative(360f)
+                verticalLineToRelative(80f)
+                horizontalLineTo(160f)
+                quadToRelative(-33f, 0f, -56.5f, -23.5f)
+                reflectiveQuadTo(80f, 720f)
+                verticalLineToRelative(-480f)
+                quadToRelative(0f, -33f, 23.5f, -56.5f)
+                reflectiveQuadTo(160f, 160f)
+                horizontalLineToRelative(640f)
+                quadToRelative(33f, 0f, 56.5f, 23.5f)
+                reflectiveQuadTo(880f, 240f)
+                verticalLineToRelative(280f)
+                horizontalLineToRelative(-80f)
+                verticalLineToRelative(-200f)
+                close()
+                moveToRelative(0f, -80f)
+                lineToRelative(320f, -200f)
+                horizontalLineTo(160f)
+                close()
+                moveTo(760f, 920f)
+                lineToRelative(-56f, -56f)
+                lineToRelative(63f, -64f)
+                horizontalLineTo(600f)
+                verticalLineToRelative(-80f)
+                horizontalLineToRelative(167f)
+                lineToRelative(-64f, -64f)
+                lineToRelative(57f, -56f)
+                lineToRelative(160f, 160f)
+                close()
+                moveTo(160f, 320f)
+                verticalLineToRelative(440f)
+                verticalLineToRelative(-240f)
+                verticalLineToRelative(3f)
+                verticalLineToRelative(-283f)
+                close()
+            }
+        }.build()
+        return _requestSVG!!
+    }
+private var _logoutSVG: ImageVector? = null
+public val logoutSVG: ImageVector
+    get() {
+        if (_logoutSVG != null) {
+            return _logoutSVG!!
+        }
+        _logoutSVG = ImageVector.Builder(
+            name = "CrossCircled",
+            defaultWidth = 25.dp,
+            defaultHeight = 25.dp,
+            viewportWidth = 15f,
+            viewportHeight = 15f
+        ).apply {
+            path(
+                fill = SolidColor(Color(0xFF000000)),
+                fillAlpha = 1.0f,
+                stroke = null,
+                strokeAlpha = 1.0f,
+                strokeLineWidth = 1.0f,
+                strokeLineCap = StrokeCap.Butt,
+                strokeLineJoin = StrokeJoin.Miter,
+                strokeLineMiter = 1.0f,
+                pathFillType = PathFillType.EvenOdd
+            ) {
+                moveTo(0.877075f, 7.49988f)
+                curveTo(0.8771f, 3.8422f, 3.8422f, 0.877f, 7.4999f, 0.877f)
+                curveTo(11.1576f, 0.877f, 14.1227f, 3.8422f, 14.1227f, 7.4999f)
+                curveTo(14.1227f, 11.1575f, 11.1576f, 14.1227f, 7.4999f, 14.1227f)
+                curveTo(3.8422f, 14.1227f, 0.8771f, 11.1575f, 0.8771f, 7.4999f)
+                close()
+                moveTo(7.49991f, 1.82704f)
+                curveTo(4.3669f, 1.827f, 1.8271f, 4.3669f, 1.8271f, 7.4999f)
+                curveTo(1.8271f, 10.6329f, 4.3669f, 13.1727f, 7.4999f, 13.1727f)
+                curveTo(10.6329f, 13.1727f, 13.1727f, 10.6329f, 13.1727f, 7.4999f)
+                curveTo(13.1727f, 4.3669f, 10.6329f, 1.827f, 7.4999f, 1.827f)
+                close()
+                moveTo(9.85358f, 5.14644f)
+                curveTo(10.0488f, 5.3417f, 10.0488f, 5.6583f, 9.8536f, 5.8536f)
+                lineTo(8.20713f, 7.49999f)
+                lineTo(9.85358f, 9.14644f)
+                curveTo(10.0488f, 9.3417f, 10.0488f, 9.6583f, 9.8536f, 9.8536f)
+                curveTo(9.6583f, 10.0488f, 9.3417f, 10.0488f, 9.1465f, 9.8536f)
+                lineTo(7.50002f, 8.2071f)
+                lineTo(5.85358f, 9.85355f)
+                curveTo(5.6583f, 10.0488f, 5.3417f, 10.0488f, 5.1465f, 9.8536f)
+                curveTo(4.9512f, 9.6583f, 4.9512f, 9.3417f, 5.1465f, 9.1464f)
+                lineTo(6.79292f, 7.49999f)
+                lineTo(5.14647f, 5.85355f)
+                curveTo(4.9512f, 5.6583f, 4.9512f, 5.3417f, 5.1465f, 5.1464f)
+                curveTo(5.3417f, 4.9512f, 5.6583f, 4.9512f, 5.8536f, 5.1464f)
+                lineTo(7.50002f, 6.79289f)
+                lineTo(9.14647f, 5.14644f)
+                curveTo(9.3417f, 4.9512f, 9.6583f, 4.9512f, 9.8536f, 5.1464f)
+                close()
+            }
+        }.build()
+        return _logoutSVG!!
+    }
--- app/src/main/java/com/example/synhub/shared/views/Home.kt
+++ app/src/main/java/com/example/synhub/shared/views/Home.kt
@@ -1,10 +1,56 @@
 package com.example.synhub.shared.views
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.example.synhub.shared.components.SlideMenu
+import kotlinx.coroutines.launch
 
 @Composable
 fun Home(nav: NavHostController){
-    Text(text = "home")
+    val slideMenuState =rememberDrawerState(
+        initialValue = DrawerValue.Closed
+    )
+
+    val scope = rememberCoroutineScope()
+
+    ModalNavigationDrawer(
+        drawerState = slideMenuState,
+        drawerContent = {
+            ModalDrawerSheet {
+                SlideMenu(nav)
+            }
+        }
+    ) {
+        Scaffold (
+            modifier = Modifier.fillMaxSize(),
+        ){
+                innerPadding -> HomeScreen(modifier = Modifier.padding(innerPadding),
+            nav)
+        }
+    }
+}
+
+@Composable
+fun HomeScreen(modifier: Modifier, nav: NavHostController) {
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp)
+            .padding(top = 30.dp),
+    ){
+        Text(text = "hola")
+    }
 }
