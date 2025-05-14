20a54166000a4df3b4173dfa1d97eed57c7fe675
Al3xiel <gabriel280104casas@gmail.com>
2025-05-13 21:44:06 -0500

    feat(register): added register view.

---
+++ app/src/main/java/com/example/synhub/shared/views/Register.kt
@@ -0,0 +1,9 @@
+package com.example.synhub.shared.views
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun Register(nav: NavHostController) {
+
+}
