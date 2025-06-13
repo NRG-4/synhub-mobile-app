fa208341a639df37c2d173af355d06f2ec831709
pachieeee <sebaspacheco2003@gmail.com>
2025-06-13 17:17:39 -0500

    feat(web-service): add request endpoints to the web service to match current endpoints in the API.

--- app/src/main/java/com/example/synhub/requests/model/response/RequestsWebService.kt
+++ app/src/main/java/com/example/synhub/requests/model/response/RequestsWebService.kt
@@ -4,26 +4,44 @@ import com.example.synhub.requests.application.dto.CreateRequest
 import com.example.synhub.requests.application.dto.RequestResponse
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Path
 
 interface RequestsWebService {
-    @GET("tasks/{taskId}/request")
-    suspend fun getRequest(
-        @Path("taskId") taskId: Long
-    ): Response<RequestResponse>
-
+    // Create request
     @POST("tasks/{taskId}/request")
     suspend fun createRequest(
         @Path("taskId") taskId: Long,
         @Body request: CreateRequest
     ): Response<RequestResponse>
 
+    // Get request by task ID
+    @GET("tasks/{taskId}/request")
+    suspend fun getRequestByTaskId(
+        @Path("taskId") taskId: Long
+    ): Response<RequestResponse>
+
+    // Update request status
     @PUT("tasks/{taskId}/request/status/{status}")
     suspend fun updateRequestStatus(
         @Path("taskId") taskId: Long,
         @Path("status") status: String
     ): Response<RequestResponse>
+
+    // Delete request by task ID
+    @DELETE("tasks/{taskId}/request")
+    suspend fun deleteRequest(
+        @Path("taskId") taskId: Long
+    ): Response<Unit>
+
+    // Get all requests from a group
+    @GET("leader/group/requests")
+    suspend fun getGroupRequests(): Response<List<RequestResponse>>
+
+    // Get all requests from a member
+    @GET("leader/member/requests")
+    suspend fun getMemberRequests(): Response<List<RequestResponse>>
 }
