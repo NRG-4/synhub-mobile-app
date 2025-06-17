18f43223f25570c0f619358cef8b9371201ae917
Al3xiel <gabriel280104casas@gmail.com>
2025-06-17 13:29:17 -0500

    fix(RetrofitClient): update base URL to use the production server

--- app/src/main/java/com/example/synhub/shared/model/client/RetrofitClient.kt
+++ app/src/main/java/com/example/synhub/shared/model/client/RetrofitClient.kt
@@ -37,8 +37,9 @@ object RetrofitClient {
             }
             .build()
 
+    //https://synhub-backend.onrender.com o http://10.0.2.2:8080
     private val retrofit: Retrofit = Retrofit.Builder()
-        .baseUrl("http://10.0.2.2:8080/api/v1/")
+        .baseUrl("https://synhub-backend.onrender.com/api/v1/")
         .client(client)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
