8a4f74da74b46a8d1d0cfb19877a37947821fe8f
pachieeee <sebaspacheco2003@gmail.com>
2025-07-04 13:42:47 -0500

    fix(dto): replace task id with an appropriate object to match response, and remove member id.

--- app/src/main/java/com/example/synhub/requests/application/dto/CreateRequest.kt
+++ app/src/main/java/com/example/synhub/requests/application/dto/CreateRequest.kt
@@ -3,6 +3,5 @@ package com.example.synhub.requests.application.dto
 data class CreateRequest(
     val description: String,
     val requestType: String,
-    val taskId: Long,
-    val memberId: Long
+    val taskId: Long
 )
--- app/src/main/java/com/example/synhub/requests/application/dto/RequestResponse.kt
+++ app/src/main/java/com/example/synhub/requests/application/dto/RequestResponse.kt
@@ -1,12 +1,13 @@
 package com.example.synhub.requests.application.dto
 
+import com.example.synhub.tasks.application.dto.TaskResponse
+
 data class RequestResponse(
     val id: Long,
     val description: String,
     val requestType: String,
     val requestStatus: String,
-    val taskId: Long,
-    val memberId: Long,
+    val task: TaskResponse,
     val createdAt: String,
     val updatedAt: String
 )
