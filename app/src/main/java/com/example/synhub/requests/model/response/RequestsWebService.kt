f0cb6075ea31a1ce868fdd712237de86cbd1c9d7
pachieeee <sebaspacheco2003@gmail.com>
2025-07-08 00:30:47 -0500

    fix(web-service): update request endpoints.

--- app/src/main/java/com/example/synhub/requests/model/response/RequestsWebService.kt
+++ app/src/main/java/com/example/synhub/requests/model/response/RequestsWebService.kt
@@ -3,45 +3,41 @@ package com.example.synhub.requests.model.response
 import com.example.synhub.requests.application.dto.CreateRequest
 import com.example.synhub.requests.application.dto.RequestResponse
 import retrofit2.Response
-import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
-import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Path
 
 interface RequestsWebService {
-    // Create request
-    @POST("tasks/{taskId}/request")
-    suspend fun createRequest(
-        @Path("taskId") taskId: Long,
-        @Body request: CreateRequest
-    ): Response<RequestResponse>
-
-    // Get request by task ID
-    @GET("tasks/{taskId}/request")
-    suspend fun getRequestByTaskId(
+    // Get requests by task ID
+    @GET("tasks/{taskId}/requests")
+    suspend fun getRequestsByTaskId(
         @Path("taskId") taskId: Long
+    ): Response<List<RequestResponse>>
+
+    // Get a request by ID
+    @GET("tasks/{taskId}/requests/{requestId}")
+    suspend fun getRequestById(
+        @Path("taskId") taskId: Long,
+        @Path("requestId") requestId: Long
     ): Response<RequestResponse>
 
     // Update request status
-    @PUT("tasks/{taskId}/request/status/{status}")
+    @PUT("tasks/{taskId}/requests/{requestId}/status/{status}")
     suspend fun updateRequestStatus(
         @Path("taskId") taskId: Long?,
+        @Path("requestId") requestId: Long,
         @Path("status") status: String
     ): Response<RequestResponse>
 
     // Delete request by task ID
-    @DELETE("tasks/{taskId}/request")
+    @DELETE("tasks/{taskId}/requests/{requestId}")
     suspend fun deleteRequest(
-        @Path("taskId") taskId: Long
+        @Path("taskId") taskId: Long,
+        @Path("requestId") requestId: Long
     ): Response<Unit>
 
     // Get all requests from a group
     @GET("leader/group/requests")
     suspend fun getGroupRequests(): Response<List<RequestResponse>>
-
-    // Get all requests from a member
-    @GET("leader/member/requests")
-    suspend fun getMemberRequests(): Response<List<RequestResponse>>
 }
